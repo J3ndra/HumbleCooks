18d6b7db313f9b45c54f528371083b1b0b2c1177
Endra <endra.kartika.25@gmail.com>
2023-06-27 11:17:45 +0700

    💡 Comment seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,7 @@ class DatabaseSeeder extends Seeder
             AdminSeeder::class,
             UserSeeder::class,
             CategorySeeder::class,
-            IngredientSeeder::class,
+            // IngredientSeeder::class,
         ]);
     }
 }
